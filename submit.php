775e2ea56b0aa25b392400b52340f60cd1e91b54
thehypermagi <shamil.haqeem@gmail.com>
2021-04-30 23:53:18 +0800

    minor changes

--- submit.php
+++ submit.php
@@ -1,8 +1,28 @@
-<html>
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+  
+	<title>Register</title>
+    
+</head>
+
 <body>
+    <?php
+        echo $_POST['name'],'<br>',$_POST['nric'];
+        
+/*         if (file_exists($_POST['nric'] . '.txt')){     //Check for filename with given nric
+            echo '<h3>You are already registered!</h3>';
+        }
+        else{   //filename not found, write to file
+            
+           $content_to_write = "Name: " . $_POST['input_name'] . "\n";
+           $content_to_write .= "NRIC: " . $_POST['input_nric'] . "\n";
+           file_put_contents($_POST['input_nric'] . '.txt', $content_to_write);
+           echo '<h3>Registration is successful!</h3>';
+        } */
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
-
+    ?>
+   
 </body>
 </html>
