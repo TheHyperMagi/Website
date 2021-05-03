c291b7a0578dcb2fe00046a65888b747b531772a
Coey-55 <101232493@students.swinburne.edu.my>
2021-05-03 23:01:53 +0800

    change indentation with space in reg_check.php and page2.html

--- reg_check.php
+++ reg_check.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html class="theme-cyan cdc-page-type-content cdc-2020" lang="en-us">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <link rel="icon" href="images/syringe.png" type="image/png" />
     <link rel="stylesheet" href="home_files/bootstrap.css" />
     <link rel="stylesheet" href="home_files/app.css" />
-	
-	<title>Check Registration | COVID-19 Info Center</title>
-	<meta name="description" content="" />
+    
+    <title>Check Registration | COVID-19 Info Center</title>
+    <meta name="description" content="" />
     <meta name="keywords" content="" />
     
 </head>
 
 <body>
-	<div class="container-fluid site-title">
+    <div class="container-fluid site-title">
       <!-- <div class=""> -->
       <div class="container">
         <div class="row">
@@ -21,8 +21,8 @@
         </div>
       </div>
     </div>
-	
-	<div id="cdc-meganav-wrapper">
+    
+    <div id="cdc-meganav-wrapper">
       <div class="container" id="cdc-meganav-bar">
         <nav class="navbar navbar-expand-xl yamm">
           <ul class="nav navbar-nav nav-justified w-100">
@@ -41,53 +41,50 @@
             <li class="nav-item" id="menu3">
               <a class="nav-link" href="page3.html"> J&J/Janssen </a>
             </li>
-			
-			<li class="nav-item" id="menu4">
-			  <a class="nav-link" href="registration.html"> Registration </a>
-			</li>
+            
+            <li class="nav-item" id="menu4">
+              <a class="nav-link" href="registration.html"> Registration </a>
+            </li>
 
-			<li class="nav-item" id="menu5">
-			  <a class="nav-link active" href="check_registration.html"> Check Registration </a>
-			</li>
+            <li class="nav-item" id="menu5">
+              <a class="nav-link active" href="check_registration.html"> Check Registration </a>
+            </li>
           </ul>
         </nav>
       </div>
     </div>
 	
-	<!-- Page Content Wrap -->
-	<div class="container d-flex flex-wrap body-wrapper bg-white">
-		<!-- Content -->
-		<main class="col-xl-9 order-xl-2">
-			<div class="row">
-				<img
-					src="images/checkRegistrationStatusBanner.png"
-					alt="Check Registration Status Banner"
-					class="img-fluid"
-				/>
-				<h2 id="content" class="open d-lg-block">
-					Check Registration Status
-				</h2>
-			</div>
-			
-			<div class="col-md-12">
-				<?php
-					
-					// echo $_POST['input_nric'];
-					
-					if (file_exists($_POST['input_nric'] . '.txt')){
-						echo '<h3>You are already registered!</h3><br>';
-					}
-					else{
-						echo '<h3>You have not registered!</h3>';
-						echo '<a href ="register.html">Please register here</a><br><br><br>';
-					}
-
-				?>
-			</div>
-		</main>
-	</div>
-	
-   <div class="container-fluid">
+    <!-- Page Content Wrap -->
+    <div class="container d-flex flex-wrap body-wrapper bg-white">
+        <!-- Content -->
+        <main class="col-xl-9 order-xl-2">
+            <div class="row">
+                <img
+                    src="images/checkRegistrationStatusBanner.png"
+                    alt="Check Registration Status Banner"
+                    class="img-fluid"
+                />
+                <h2 id="content" class="open d-lg-block">
+                    Check Registration Status
+                </h2>
+            </div>
+            
+            <div class="col-md-12">
+                <?php
+                    // echo $_POST['input_nric'];
+                    if (file_exists($_POST['input_nric'] . '.txt')) {
+                       echo '<h3>You are already registered!</h3><br>';
+                    }
+                    else{
+                        echo '<h3>You have not registered!</h3>';
+                        echo '<a href ="register.html">Please register here</a><br><br><br>';
+                    }
+                ?>
+            </div>
+        </main>
+    </div>
+    
+    <div class="container-fluid">
       <div class="container">
         <div class="card-body bg-quaternary">
           <h5>Disclaimer</h5>
@@ -104,6 +101,6 @@
         </div>
       </div>
     </div>
-	
+    
 </body>
 </html>
